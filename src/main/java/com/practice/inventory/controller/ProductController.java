5d9a380ec64d7dab4069d502c3920e71154d48d6
Deepak Bhoria <dpk22dev@gmail.com>
2023-04-27 11:32:03 +0530

    controller methods for product

--- src/main/java/com/practice/inventory/controller/ProductController.java
+++ src/main/java/com/practice/inventory/controller/ProductController.java
@@ -3,8 +3,10 @@ package com.practice.inventory.controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.validation.annotation.Validated;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -24,13 +26,18 @@ public class ProductController {
   ProductRepository productRepository;
 
   @GetMapping
-  public ResponseEntity<Product> getProduct() {
-    Optional<Product> Product = Optional.empty();
+  public ResponseEntity<List<Product>> getProducts() {
 
-    Product = Optional.of(new Product());
+    List<Product> products = (List<Product>) productRepository.findAll();
+    return ResponseEntity.ok(products);
+  }
 
-    if (Product.isPresent()) {
-      return ResponseEntity.ok(Product.get());
+  @GetMapping("{id}")
+  public ResponseEntity<Product> getProductById(@PathVariable("id") Long id) {
+    Optional<Product> item = productRepository.findById(id);
+
+    if (item.isPresent()) {
+      return ResponseEntity.ok(item.get());
     } else {
       return ResponseEntity.notFound().build();
     }
@@ -45,6 +52,23 @@ public class ProductController {
       newProduct.setProductDesc(item.getProductDesc());
       Product savedProduct = productRepository.save(newProduct);
       return ResponseEntity.ok(savedProduct);
+    } catch (Exception e) {
+      return ResponseEntity.internalServerError().build();
+    }
+  }
+
+  @PutMapping("{id}")
+  public ResponseEntity<Product> updateProduct(@NonNull @PathVariable("id") Long id, @RequestBody ProductDto item) {
+    try {
+      Optional<Product> existingProduct = productRepository.findById(id);
+      if (existingProduct.isPresent()) {
+        existingProduct.get().setProductName(item.getProductName());
+        existingProduct.get().setProductDesc(item.getProductDesc());
+        Product savedProduct = productRepository.save(existingProduct.get());
+        return ResponseEntity.ok(savedProduct);
+      } else {
+        return ResponseEntity.notFound().build();
+      }
     } catch (Exception e) {
       return ResponseEntity.internalServerError().build();
     }
